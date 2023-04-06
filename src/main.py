fe75ea71f4f421877ad3725fdd74feda32c711d3
Vishnu Kolal <vkolal1@jhu.edu>
2023-04-06 18:02:39 -0400

    working base movement code, tested in simulation

--- src/main.py
+++ src/main.py
@@ -21,69 +21,16 @@ from moveit_commander.conversions import pose_to_list
 def init_ros():
     
     rospy.init_node('hri_node', anonymous=False)
-
-def init_ros_publishers():
     
-    base_vel_publisher = rospy.Publisher('/mobile_base_controller/cmd_vel',Twist,queue_size=1)
-
-
-    return base_vel_publisher
-
-def base_command(lin,ang):
-    move_cmd = Twist()
-    move_cmd.linear.x = lin[0]
-    move_cmd.linear.y = lin[1]
-    move_cmd.linear.z = lin[2] # m/s
-    move_cmd.angular.x = ang[0]
-    move_cmd.angular.y = ang[1]
-    move_cmd.angular.z = ang[2]
-
-    return move_cmd
-
-
-
 
 if __name__=='__main__':
     init_ros()
-    
-    #p,joy=init_ros_publishers()
 
-    #joy_cmd = Bool()
-    #joy_cmd.data = False
-
-    #fwd=base_command([0.2,0.0,0.0],[0.0,0.0,0.0])
-    #stop=base_command([0.0,0.0,0.0],[0.0,0.0,0.0])
-    #left=base_command([0.0,0.0,0.0],[0.0,0.0,-0.2])
-    
-    #now = rospy.Time.now()
-    #rate = rospy.Rate(60)
-    
-    #while rospy.Time.now() < now + rospy.Duration.from_sec(7):
-
-        #joy.publish(joy_cmd)
-        #p.publish(fwd)
-        #rate.sleep() 
-
-    #p.publish(stop)
-
-    #now = rospy.Time.now()
-
-    #while rospy.Time.now() < now + rospy.Duration.from_sec(7.85):
-        #joy.publish(joy_cmd)
-        #p.publish(left)
-        #rate.sleep() 
-
-    #p.publish(stop)
-
-    #now = rospy.Time.now()
-
-    #while rospy.Time.now() < now + rospy.Duration.from_sec(7):
-
-        #joy.publish(joy_cmd)
-        #p.publish(fwd)
-        #rate.sleep()  
-
-    #p.publish(stop)
+    base = move_base.robot_base()
+    base.move_from_init_to_home()
+    base.move_from_home_to_inv()
+    base.move_from_inv_to_tar()
+    base.move_from_tar_to_home()
 
 
 
